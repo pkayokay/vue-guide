f545bbf3fcc273b8d10803b501fd8c79ba3b5a83
Paul Kim <pkayokay@gmail.com>
2018-06-25 23:12:50 -0400

    Example of a Vue Component

--- vue.js
+++ vue.js
@@ -49,3 +49,22 @@ var app6 = new Vue({
     message: 'Hello Vue!'
   }
 })
+
+
+Vue.component('todo-item', {
+  // The todo-item acceps a prop ( a custom attribute).
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables'},
+      { id: 1, text: 'Cheese'},
+      { id: 2, text: 'Apples'}
+    ]
+  }
+})
+
