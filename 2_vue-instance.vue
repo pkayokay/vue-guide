5e7c779007bdec9db62395f6b20eaca98cf2f730
Paul Kim <pkayokay@gmail.com>
2018-06-30 18:48:59 -0400

    Add template syntax - interpolation notes

--- 2_vue-instance.vue
+++ 2_vue-instance.vue
@@ -1,4 +1,4 @@
-// Data and Methods
+Data and Methods
   // Data object
   var data = { a: 1 }
   // Object is added to a Vue instance
@@ -18,16 +18,16 @@
   // In addition to data properties, Vue instances expose a number of useful instance properties and methods, prefixed with $ to differentiate them from user-defined properties (Reference the API for these).
 
 
-// Instance Lifecycle Hooks
-  // Each Vue instnace goes through a series of initialization steps when it's created.
-    // 1. Needs to set up data observation
-    // 2. Compile the template
-    // 3. Mount the instnce to the DOM
-    // 4. Update the DOM when data changes
+Instance Lifecycle Hooks
+  Each Vue instnace goes through a series of initialization steps when it's created.
+    1. Needs to set up data observation
+    2. Compile the template
+    3. Mount the instnce to the DOM
+    4. Update the DOM when data changes
 
-  // Lifecycle hooks: gives users the ability to add their own code at specific stages of the instance.
+  Lifecycle hooks: gives users the ability to add their own code at specific stages of the instance.
 
-// Example, the 'created' hook can be used to run code after the creation of an instance
+Example, the 'created' hook can be used to run code after the creation of an instance
 
 new Vue({
   data: {
@@ -38,8 +38,9 @@ new Vue({
   }
 })
 // => "a is: 1"
-// There's also mounted, updated, destroyed, all of these lifecycle hooks are called with their 'this' context pointing to the Vue instance invoking it.
 
-// Reference the Lifecycle Diagram for an overview
-// https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
+There's also mounted, updated, destroyed, all of these lifecycle hooks are called with their 'this' context pointing to the Vue instance invoking it.
+
+Reference the Lifecycle Diagram for an overview
+https://vuejs.org/v2/guide/instance.html#Lifecycle-Diagram
 
---
+++ 3_template-syntax.vue
@@ -0,0 +1,39 @@
+Interpolations
+  # Text
+  <span>{{ message }}</span>
+
+  The mustache tag will be replaced with the value of message, and will be updated whenever the property changes.
+
+  There is an option to do one-time interpolations and not update data.
+  <span v-once>This will never change: {{ message }}</span>
+
+
+  # Raw HTML
+
+  Mustaches interpret dat as plain text, in order to output real HTML we will need the v-html directive:
+
+  <p>Using mustaches: {{ rawHTML }}</p>
+  // Using mustaches: <span style="color: red">This should be red.</span>
+
+  <p>Using v-html directive: <span v-html="rawHTML"></span></p>
+  // Using v-html directive: This should be red.
+
+
+  # Attributes
+
+  Mustaches cannot be used inside HTML attributes, use a v-bind directive:
+  <div v-bind:id="dynamicId"></div>
+
+
+  # Using JavaScript Expressions
+
+  Vue supports the full power of JavScript expressions
+
+  {{ number + 1}}
+  {{ ok ? 'YES' : 'NO'}}
+  {{ message.split('').reverse().join('') }}
+  <div v-bind:id="'list-' + id"></div>
+
+  The expressions will be evaluated as JavaScript in the data scope of the owner Vue instance, one restriction is that each binding can only contain one single expression.
+
+
