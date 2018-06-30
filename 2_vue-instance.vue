8ab42cfe30a3176eb270d6d88791fc4aa6d65091
Paul Kim <pkayokay@gmail.com>
2018-06-30 18:12:47 -0400

    Add lifecycle hooks notes

--- 2_vue-instance.vue
+++ 2_vue-instance.vue
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Vue Instnce</title>
-  <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-  <style>
-    body {
-      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-  </style>
-</head>
-<body>
-<script type="text/javascript">
-
 // Data and Methods
   // Data object
   var data = { a: 1 }
@@ -30,6 +16,30 @@
 
   // There's only one exception to this with the use of Object.freeze()
   // In addition to data properties, Vue instances expose a number of useful instance properties and methods, prefixed with $ to differentiate them from user-defined properties (Reference the API for these).
-</script>
-</body>
-</html>
+
+
+// Instance Lifecycle Hooks
+  // Each Vue instnace goes through a series of initialization steps when it's created.
+    // 1. Needs to set up data observation
+    // 2. Compile the template
+    // 3. Mount the instnce to the DOM
+    // 4. Update the DOM when data changes
+
+  // Lifecycle hooks: gives users the ability to add their own code at specific stages of the instance.
+
+// Example, the 'created' hook can be used to run code after the creation of an instance
+
+new Vue({
+  data: {
+    a: 1
+  },
+  created: function() {
+    console.log('a is: ' + this.a)
+  }
+})
+// => "a is: 1"
+// There's also mounted, updated, destroyed, all of these lifecycle hooks are called with their 'this' context pointing to the Vue instance invoking it.
+
+
+
+
